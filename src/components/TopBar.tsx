7547fcd49ad6ce44762dba30604d149ea4d0ca0b
cryptomafiaPB <jugad360@gmail.com>
2024-07-26 19:58:25 +0530

    Update TopBar.tsx
    added padding left 6 "pl-6" to <p> tag note section

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -7,7 +7,7 @@ export default function TopBar() {
   return (
     <div className="w-full  flex items-center justify-between px-8 border-b h-16 fixed z-10 top-0 bg-zinc-100 sm:h-20">
       <ComboBox />
-      <p className=" leading-5 font-mono text-sm [&:not(:first-child)] hidden sm:ml-52 lg:block  md:ml-56 pr-16">
+      <p className="pl-6 leading-5 font-mono text-sm [&:not(:first-child)] hidden sm:ml-52 lg:block  md:ml-56 pr-16">
         Note : All the data belongs to cricbuzz.com. Please do not use it for
         commercial purposes. This project is only for educational purposes.
       </p>
